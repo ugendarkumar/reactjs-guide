721a799109306d226a7b88f9deb7e266d22c4a7a
Ugendarkumar J P <ugendarkumar_j@photoninfotech.net>
2018-12-07 14:52:22 +0530

    delete event bug fix

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,7 +3,7 @@ import  React from "react";
 import Person from './Person/Person';
 
 const Persons = (props) => props.persons.map((person,index) => {
-            return (<Person click = {(event) => props.clicked(event,index)} 
+            return (<Person click = {(event) => props.clicked(index)} 
             change = {(event) => props.changed(event,person.id)}
             name = {person.name} age = {person.age}
             key = {person.id}> </Person>                   
